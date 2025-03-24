826a47c1e0e9efbc783ad9276e56daaa4008218a
TUSHAR SWAMI <tusharswami17@gamil.com>
2025-03-24 16:28:21 +0530

    add width

--- src/components/Opportunity-Overview/OpportunityOverview.jsx
+++ src/components/Opportunity-Overview/OpportunityOverview.jsx
@@ -72,22 +72,22 @@ const OpportunityOverview = () => {
     }, [])
     return (
         <div ref={componentRef}>
-            <div className='flex this-section px-3 justify-center min-h-screen items-center overflow-hidden py-10 max-sm:py-5 bg-light-black'>
-                <div className='max-w-[1143px] max-xl:max-w-[848px] w-full container relative'>
+            <div className='flex this-section px-3 justify-center min-h-screen items-center overflow-hidden max-sm:py-5 bg-light-black'>
+                <div className='max-w-[1060px] max-xl:max-w-[848px] w-full container relative'>
                     <h3 className=' absolute top-1/2 left-1/2 -translate-x-1/2 animate-bounce text-center animation-text -translate-y-1/2 font-semibold max-[872px]:hidden text-4xl text-white leading-[130%]'>Scroll Down To Show Animation</h3>
-                    <h2 className='font-semibold text-[46px] max-lg:text-4xl max-md:text-3xl max-sm:text-2xl max-[872px]:text-center text-white leading-[130%] min-[872px]:-translate-y-96 mb-9 max-lg:mb-8 max-md:mb-7 max-sm:mb-6 heading'>Opportunity Overview</h2>
+                    <h2 className='font-semibold text-[46px] max-lg:text-4xl max-md:text-3xl max-sm:text-2xl max-[872px]:text-center text-white leading-[130%] min-[872px]:-translate-y-96 mb-8 max-lg:mb-7 max-md:mb-6 max-sm:mb-5 heading'>Opportunity Overview</h2>
                     <div className="w-full flex flex-col justify-center items-center gap-4 max-md:gap-2">
                         <div className='flex flex-wrap w-full gap-4 max-md:gap-2 max-xl:max-w-[848px] justify-center items-center'>
-                            <Image className='card-image min-[872px]:translate-x-[1450px] max-xl:w-[200px] max-[441px]:!size-[130px] max-[770px]:!size-[165px] max-xl:h-[220px] min-[872px]:rotate-15' src={'/assets/image/opportunity.png'} alt='images' width={273} height={270} />
-                            <Image className='card-image min-[872px]:translate-x-[1450px] max-xl:w-[200px] max-[441px]:!size-[130px] max-[770px]:!size-[165px] max-xl:h-[220px] min-[872px]:rotate-15' src={'/assets/image/status.png'} alt='images' width={273} height={270} />
-                            <Image className='card-image min-[872px]:translate-x-[1450px] max-xl:w-[200px] max-[441px]:!size-[130px] max-[770px]:!size-[165px] max-xl:h-[220px] min-[872px]:rotate-15' src={'/assets/image/track-record.png'} alt='images' width={273} height={270} />
-                            <Image className='card-image min-[872px]:translate-x-[1450px] max-xl:w-[200px] max-[441px]:!size-[130px] max-[770px]:!size-[165px] max-xl:h-[220px] min-[872px]:rotate-15' src={'/assets/image/leadership.png'} alt='images' width={273} height={270} />
+                            <Image className='card-image min-[872px]:translate-x-[1750px] max-xl:w-[200px] max-[441px]:!size-[130px] max-[770px]:!size-[165px] max-xl:h-[220px] min-[872px]:rotate-15' src={'/assets/image/opportunity.png'} alt='images' width={253} height={250} />
+                            <Image className='card-image min-[872px]:translate-x-[1750px] max-xl:w-[200px] max-[441px]:!size-[130px] max-[770px]:!size-[165px] max-xl:h-[220px] min-[872px]:rotate-15' src={'/assets/image/status.png'} alt='images' width={253} height={250} />
+                            <Image className='card-image min-[872px]:translate-x-[1750px] max-xl:w-[200px] max-[441px]:!size-[130px] max-[770px]:!size-[165px] max-xl:h-[220px] min-[872px]:rotate-15' src={'/assets/image/track-record.png'} alt='images' width={253} height={250} />
+                            <Image className='card-image min-[872px]:translate-x-[1750px] max-xl:w-[200px] max-[441px]:!size-[130px] max-[770px]:!size-[165px] max-xl:h-[220px] min-[872px]:rotate-15' src={'/assets/image/leadership.png'} alt='images' width={253} height={250} />
                         </div>
-                        <div className='flex flex-wrap w-full max-md:gap-2 flex-row-reverse justify-center items-center gap-4 max-xl:max-w-[848px]'>
-                            <Image className='card-image-2 max-xl:w-[200px] max-[441px]:!size-[130px] max-[770px]:!size-[165px] max-xl:h-[220px] min-[872px]:-translate-x-[1450px] min-[872px]:-rotate-15' src={'/assets/image/vertically-integrated.png'} alt='images' width={273} height={270} />
-                            <Image className='card-image-2 max-xl:w-[200px] max-[441px]:!size-[130px] max-[770px]:!size-[165px] max-xl:h-[220px] min-[872px]:-translate-x-[1450px] min-[872px]:-rotate-15' src={'/assets/image/design.png'} alt='images' width={273} height={270} />
-                            <Image className='card-image-2 max-xl:w-[200px] max-[441px]:!size-[130px] max-[770px]:!size-[165px] max-xl:h-[220px] min-[872px]:-translate-x-[1450px] min-[872px]:-rotate-15' src={'/assets/image/pricing-innovation.png'} alt='images' width={273} height={270} />
-                            <Image className='card-image-2 max-xl:w-[200px] max-[441px]:!size-[130px] max-[770px]:!size-[165px] max-xl:h-[220px] min-[872px]:-translate-x-[1450px] min-[872px]:-rotate-15' src={'/assets/image/scalable-growth.png'} alt='images' width={273} height={270} />
+                        <div className='flex flex-wrap max-md:gap-2 w-full flex-row-reverse justify-center items-center gap-4 max-xl:max-w-[848px]'>
+                            <Image className='card-image-2 max-xl:w-[200px] max-[441px]:!size-[130px] max-[770px]:!size-[165px] max-xl:h-[220px] min-[872px]:-translate-x-[1750px] min-[872px]:-rotate-15' src={'/assets/image/vertically-integrated.png'} alt='images' width={253} height={250} />
+                            <Image className='card-image-2 max-xl:w-[200px] max-[441px]:!size-[130px] max-[770px]:!size-[165px] max-xl:h-[220px] min-[872px]:-translate-x-[1750px] min-[872px]:-rotate-15' src={'/assets/image/design.png'} alt='images' width={253} height={250} />
+                            <Image className='card-image-2 max-xl:w-[200px] max-[441px]:!size-[130px] max-[770px]:!size-[165px] max-xl:h-[220px] min-[872px]:-translate-x-[1750px] min-[872px]:-rotate-15' src={'/assets/image/pricing-innovation.png'} alt='images' width={253} height={250} />
+                            <Image className='card-image-2 max-xl:w-[200px] max-[441px]:!size-[130px] max-[770px]:!size-[165px] max-xl:h-[220px] min-[872px]:-translate-x-[1750px] min-[872px]:-rotate-15' src={'/assets/image/scalable-growth.png'} alt='images' width={253} height={250} />
                         </div>
                     </div>
                 </div>

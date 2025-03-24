f81e2341fa84c5bd97e890f9095ed71da9731289
TUSHAR SWAMI <tusharswami17@gamil.com>
2025-03-24 13:55:41 +0530

    add image

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -5,7 +5,7 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
-import { Live_Tracking_Description, Real_Time_Description, Real_Time_DescriptionFour, Real_Time_DescriptionThree, Real_Time_DescriptionTwo } from '@/utils/helper';
+import { Bookkeeping_Description, Bookkeeping_Description_Two, Insight_Description, Invoicing, Live_Tax_Blue_Description, Live_Tax_Blue_Description_Three, Live_Tax_Blue_Description_Two, Live_Tax_Pink_Description, Live_Tax_Pink_Description_Three, Live_Tax_Pink_Description_Two, Live_Tracking_Description, Multiple_Income_Description, Multiple_Income_Description_Three, Multiple_Income_Description_Two, Real_Time_Description, Real_Time_DescriptionFour, Real_Time_DescriptionThree, Real_Time_DescriptionTwo } from '@/utils/helper';
 
 const HomePage = () => {
     const componentRef = useRef(null);
@@ -13,15 +13,14 @@ const HomePage = () => {
         const ctx = gsap.context(() => {
             const mm = gsap.matchMedia();
             const tl = gsap.timeline();
-            mm.add("(min-width: 1024px)", () => {
+            mm.add("(min-width: 1023.98px)", () => {
                 ScrollTrigger.create({
-                    trigger: ".pin-section",
-                    start: "top , top",
+                    trigger: ".sm-pin",
+                    start: "top top",
                     end: "800% bottom",
                     scrub: 1,
                     pin: true,
                     animation: tl,
-                    markers: true,
                 });
 
                 tl.to(".section-two", {
@@ -34,18 +33,30 @@ const HomePage = () => {
                     top: "0%",
                 }, 2)
             })
-            mm.add("(max-width: 1024px)", () => {
+            mm.add("(max-width: 1023.98px) and (min-width: 767.98px)", () => {
                 ScrollTrigger.create({
-                    trigger: ".pin-section",
-                    start: "top , top",
-                    end: "bottom bottom",
+                    trigger: ".sm-pin",
+                    start: "top top",
+                    end: "800% top",
                     scrub: 1,
                     pin: true,
                     animation: tl,
-                    markers: true,
                 });
                 tl.to(".pin-section", {
-                    yPercent: -50
+                    yPercent: -80
+                })
+            })
+            mm.add("(max-width: 767.98px) ", () => {
+                ScrollTrigger.create({
+                    trigger: ".sm-pin",
+                    start: "top top",
+                    end: "800% top",
+                    scrub: 1,
+                    pin: true,
+                    animation: tl,
+                });
+                tl.to(".pin-section", {
+                    yPercent: -85
                 })
             })
         }, componentRef);
@@ -53,11 +64,13 @@ const HomePage = () => {
     }, [])
     return (
         <div ref={componentRef}>
-            <div className='pin-section min-h-screen relative max-xl:px-4 overflow-hidden'>
-                <CommonSection sectionClass={'section-one lg:absolute left-1/2 lg:-translate-x-1/2 w-full'} titleOne={'Real Time Tax'} description={Real_Time_Description} descriptionTwo={Real_Time_DescriptionTwo} descriptionThree={Real_Time_DescriptionThree} descriptionFour={Real_Time_DescriptionFour} titleTwo={'Live tracking of the tax return'} secondDescription={Live_Tracking_Description} />
-                <CommonSection sectionClass={'section-two lg:absolute left-1/2 top-full lg:-translate-x-1/2 w-full'} titleOne={'Real Time Tax'} dataClass={'!bg-purple'} titleClass={'!text-gray'} descriptionClass={'!text-white'} description={Real_Time_Description} descriptionTwo={Real_Time_DescriptionTwo} descriptionThree={Real_Time_DescriptionThree} descriptionFour={Real_Time_DescriptionFour} titleTwo={'Live tracking of the tax return'} secondDescription={Live_Tracking_Description} />
-                <CommonSection sectionClass={'section-three lg:absolute left-1/2 top-full lg:-translate-x-1/2 w-full'} titleOne={'Real Time Tax'} dataClass={'!bg-pink'} description={Real_Time_Description} descriptionTwo={Real_Time_DescriptionTwo} descriptionThree={Real_Time_DescriptionThree} descriptionFour={Real_Time_DescriptionFour} titleTwo={'Live tracking of the tax return'} secondDescription={Live_Tracking_Description} />
-                <CommonSection sectionClass={'section-four lg:absolute left-1/2 top-full lg:-translate-x-1/2 w-full'} titleOne={'Real Time Tax'} dataClass={'!bg-blue'} titleClass={'!text-gray'} descriptionClass={'!text-white'} description={Real_Time_Description} descriptionTwo={Real_Time_DescriptionTwo} descriptionThree={Real_Time_DescriptionThree} descriptionFour={Real_Time_DescriptionFour} titleTwo={'Live tracking of the tax return'} secondDescription={Live_Tracking_Description} />
+            <div className='lg:px-4 sm-pin max-[1023px]:h-screen max-[1023px]:overflow-hidden' >
+                <div className='pin-section min-h-screen relative max-xl:px-4 overflow-hidden'>
+                    <CommonSection sectionImage={'/assets/image/first-section-image.png'} bgImage={'bg-[url("/assets/image/first-section-bg-image.png")]'} sectionClass={'section-one lg:absolute left-1/2 lg:-translate-x-1/2 w-full'} titleOne={'Real Time Tax'} description={Real_Time_Description} descriptionTwo={Real_Time_DescriptionTwo} descriptionThree={Real_Time_DescriptionThree} descriptionFour={Real_Time_DescriptionFour} titleTwo={'Live tracking of the tax return'} secondDescription={Live_Tracking_Description} />
+                    <CommonSection sectionImage={'/assets/image/second-section-image.png'} bgImage={'bg-[url("/assets/image/second-section-bg-image.png")]'} sectionClass={'section-two lg:absolute left-1/2 top-full lg:-translate-x-1/2 w-full'} titleOne={'Multiple Incomes in one place'} dataClass={'!bg-purple'} titleClass={'!text-gray'} descriptionClass={'!text-white'} description={Multiple_Income_Description} descriptionTwo={Multiple_Income_Description_Two} descriptionThree={Multiple_Income_Description_Three} titleTwo={'Live tax admin support'} secondDescription={Live_Tax_Blue_Description} secondDescriptionTwo={Live_Tax_Blue_Description_Two} secondDescriptionThree={Live_Tax_Blue_Description_Three} />
+                    <CommonSection sectionImage={'/assets/image/third-section-image.png'} bgImage={'bg-[url("/assets/image/third-section-bg-image.png")]'} sectionClass={'section-three lg:absolute left-1/2 top-full lg:-translate-x-1/2 w-full'} titleOne={'Bookkeeping'} dataClass={'!bg-pink'} description={Bookkeeping_Description} descriptionTwo={Bookkeeping_Description_Two} titleTwo={'Live tax admin support'} secondDescription={Live_Tax_Pink_Description} secondDescriptionTwo={Live_Tax_Pink_Description_Two} secondDescriptionThree={Live_Tax_Pink_Description_Three} />
+                    <CommonSection sectionImage={'/assets/image/fourth-section-image.png'} bgImage={'bg-[url("/assets/image/fourth-section-bg-image.png")]'} sectionClass={'section-four lg:absolute left-1/2 top-full lg:-translate-x-1/2 w-full'} titleOne={'Invoicing'} dataClass={'!bg-blue'} titleClass={'!text-gray'} descriptionClass={'!text-white'} description={Invoicing} titleTwo={'Insights on your income'} secondDescription={Insight_Description} />
+                </div>
             </div>
         </div>
     )
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,10 +1,53 @@
 export const Real_Time_Description =
   "Our app will give you your live tax position which will be the actual tax amount owed across multiple income streams, to date. There will be no need to wait until the end of the year to see what you owe, taking all of the anxiety of the unknown away";
+
 export const Real_Time_DescriptionTwo =
   "All incoming and outgoing transactions will be updated in real time, along with any deductions or credits, which means users will have the most up to date figure every time they open the app.";
+
 export const Real_Time_DescriptionThree =
   "For most people their tax bill can be their biggest outgoing. Waiting up to 20 months from the point of income to finding out the final tax bill can make things hard to plan";
+
 export const Real_Time_DescriptionFour =
   "And with HMRC’s Making Tax Digital initiative, businesses will soon be required to report their income every quarter - making up to date record keeping a necessity";
+
 export const Live_Tracking_Description =
   "Once submitted, users will be kept in the loop on the progress of their return, every step of the way. Our system is integrated with HMRC and alerts will be sent to the user to take action where and when required.";
+
+export const Multiple_Income_Description =
+  "Importing income and outgoings across multiple income streams is currently not possible on any other software, and our app will automatically highlight expenses eligible for a refund.";
+
+export const Multiple_Income_Description_Two =
+  "Multiple incomes can include anything from salary, property, capital gains, self employment, overseas income, dividends & interest or tax credits.";
+
+export const Multiple_Income_Description_Three =
+  "We’ll show the tax breakdown per income type and our app will even calculate an overall aggregated tax calculation.";
+
+export const Live_Tax_Blue_Description =
+  "Users on the premium subscription tier will be paired with a tax advisor to help them with the entire process. Our experts will help plan their business and taxes to make sure they stay as tax efficient as possible. We will use our expertise to either save them money on their tax bill or get the best possible tax rebate so that they can keep more money in their pockets.";
+
+export const Live_Tax_Blue_Description_Two =
+  "Our in-app function will enable them to ask questions and get real time answers on any tax issues they may be facing, with sector specific tax assistants on hand to provide valuable insight around the relevant tax situation.";
+
+export const Live_Tax_Blue_Description_Three =
+  "We’ll show the tax breakdown per income type and our app will even calculate an overall aggregated tax calculation";
+
+export const Bookkeeping_Description =
+  "We’ll enable people to reconcile all of their transactions in one place. Our app will import and categorise bank feeds to determine which of the expenses qualify. That means less time spent worrying about tax rules and more time spent doing the things they enjoy.";
+
+export const Bookkeeping_Description_Two =
+  " Our banking level secure encryption cloud storage, will store expense receipts in order to ensure that the tax return is fully compliant in case of any HMRC enquiries.";
+
+export const Live_Tax_Pink_Description =
+  "All of the income and outgoings will be tracked so that the self assessment income reporting can be completed in the app.";
+
+export const Live_Tax_Pink_Description_Two =
+  "Because Pie is a registered tax agent with HMRC, it will be able to send off your self-assessments directly to HMRC without needing to leave or download the data outside of the app.";
+
+export const Live_Tax_Pink_Description_Three =
+  "Users will also have the option to pay a £59 checking fee to have their return reviewed and checked by tax assistants, that will have been trained and certified by our in house CIMA qualified accountancy practice.";
+
+export const Invoicing =
+  "Pie will allow users to send digital invoices to their customers, on the go. There will be one place to manage all clients and invoicing, allowing them to run their business whilst getting paid faster. Invoice payments will be trackable with automated payment reminders to debtors. This section will also allow users to view monthly revenue and track year on year changes";
+
+export const Insight_Description =
+  "Our app will analyse data and provide insights into things like the biggest expenses and how these fluctuate on a monthly or yearly basis and even tell users how they’re performing in comparison to their peers and highlight any areas where they could improve efficiency";
